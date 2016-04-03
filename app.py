5a106b2f32003cfeafc13f37bfe9581ce96c407b
Calum Gilchrist <calum@fanduel.com>
2016-04-03 15:01:21 +0100

    Move Fixture into Module

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
 from flask import Flask, render_template, abort
+from whatscooking.fixtures import recipe_list
 
 app = Flask(__name__)
-
-
-recipe_list = [{
-    'name': 'spaghetti bolognese',
-    # Can we get the ingredients from the text body?
-    'ingredients': [
-        'mince',
-        'spaghetti',
-        'pasata',
-        'oregano',
-        'garlic',
-        'onion'
-    ],
-    'steps': [
-        "Add Oil to a Pan and heat\n",
-        "Add mince and onions to the pan and cook until ",
-        "browned, stirring occasionally\n",
-        "Add pasata, oregano and garlic\n",
-        "Boil pasta in a second pot\n"
-        "When pasta is cooking mix into the bolognese"
-    ]
-}]
 
 
 @app.route('/')
